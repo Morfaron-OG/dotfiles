ca9b12dc33857720b02838ee75399f195c4a4302
Morfaron <nona@yo.biz>
2019-10-13 21:17:53 -0700

    added dracula theme to qutebrowser

---
+++ .config/qutebrowser/config.py
@@ -0,0 +1,13 @@
+config.load_autoconfig()
+
+import dracula.draw
+dracula.draw.blood(c, {
+    'spacing': {
+        'vertical': 0,
+        'horizontal': 0
+    },
+    'font': {
+        'family': 'Menlo, Terminus, Monaco, Monospace',
+        'size': 12
+    }
+})
